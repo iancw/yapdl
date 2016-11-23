e26a75beb10edb09d4289b004ef971c1b2fa6021
Ian Will <icwill@gmail.com>
2016-11-22 22:56:38 -0500

    Add find and filter functions

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict';
 
 const getIncidents = require('./lib/get-incidents');
+const findIncidents = require('./lib/find-incidents');
+const filterIncidents = require('./lib/filter-incidents');
 
 module.exports = {
     getIncidents: getIncidents,
+    findIncidents: findIncidents,
+    filterIncidents: filterIncidents,
 };
+
